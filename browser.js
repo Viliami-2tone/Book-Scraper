78df4301a89083a6331db0278bd4497db3224e51
Viliami <viliamitutone@developersinsitute.co.nz>
2021-06-14 23:01:50 +1200

    reverted to the other stuff

--- browser.js
+++ browser.js
@@ -5,8 +5,8 @@ async function startBrowser(){
     try {
         console.log("Opening the browser......");
         browser = await puppeteer.launch({
-            headless: true,
-            //args:k ["--disable-setuid-sandbox"],
+            headless: false,
+            args: ["--disable-setuid-sandbox"],
             'ignoreHTTPSErrors': true
         });
     } catch (err) {
