a01ad402d946ad7276508efe7458e6ac8edf4ee2
Viliami Tutone <viliamitutone@developersinstitute.co.nz>
2021-06-15 18:16:28 +1200

    Small changes to stuff

--- pageScraper.js
+++ pageScraper.js
@@ -7,25 +7,21 @@ const scraperObject = {
         await page.goto(this.url);
         // Wait for the required DOM to be rendered
         await page.waitForSelector('.mw-parser-output');
-        // Get the link to all the required books
-        // let urls = await page.$$eval('section ol > li', links => {
-        //     // Make sure the book to be scraped is in stock
-        //     links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
-        //     // Extract the links from the data
-        //     links = links.map(el => el.querySelector('h3 > a').href)
-        //     return links;
-        // });
-
+       
         let titleData = await page.$('table#Past_events > tbody > tr > td:nth-of-type(2)')
         let linkData = await page.$('table#Past_events > tbody > tr > td:nth-of-type(2) > a')
 
         let value = await page.evaluate(el => el.textContent, titleData)
         let link = await page.evaluate(el => el.href, linkData)
 
+        await page.goto(link)
+        console.log(`Navigating to ${link}`)
 
-        console.log(value)
-        console.log(link)
+        let fighterData = await page.$('table.toccolors > tbody > tr > td')
 
+        let fighterDatatext = await page.evaluate(el => el.textContent, fighterData)
+
+        console.log(fighterDatatext);
         ;
     }
 }
